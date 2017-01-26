c31fa2b7d7ab4086fac82d39a296d4bc78af6180
b6030684 <b6030684@my.shu.ac.uk>
2017-01-26 17:41:41 +0000

    First Commit And Last Commit
    Complete Code For Application

---
+++ The_Safe/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace The_Safe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Txt_Status = new System.Windows.Forms.TextBox();
+            this.Btn_Set_Combination = new System.Windows.Forms.Button();
+            this.Test_Key = new System.Windows.Forms.Button();
+            this.Enter_Key = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Txt_Status
+            // 
+            this.Txt_Status.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Txt_Status.Location = new System.Drawing.Point(79, 31);
+            this.Txt_Status.Name = "Txt_Status";
+            this.Txt_Status.ReadOnly = true;
+            this.Txt_Status.Size = new System.Drawing.Size(113, 20);
+            this.Txt_Status.TabIndex = 0;
+            this.Txt_Status.Text = "Combination Not Set";
+            this.Txt_Status.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Txt_Status.TextChanged += new System.EventHandler(this.Txt_Status_TextChanged);
+            // 
+            // Btn_Set_Combination
+            // 
+            this.Btn_Set_Combination.Location = new System.Drawing.Point(68, 189);
+            this.Btn_Set_Combination.Name = "Btn_Set_Combination";
+            this.Btn_Set_Combination.Size = new System.Drawing.Size(136, 23);
+            this.Btn_Set_Combination.TabIndex = 1;
+            this.Btn_Set_Combination.Text = "Set The Combination";
+            this.Btn_Set_Combination.UseVisualStyleBackColor = true;
+            this.Btn_Set_Combination.Click += new System.EventHandler(this.Btn_Set_Combination_Click);
+            // 
+            // Test_Key
+            // 
+            this.Test_Key.Location = new System.Drawing.Point(95, 160);
+            this.Test_Key.Name = "Test_Key";
+            this.Test_Key.Size = new System.Drawing.Size(75, 23);
+            this.Test_Key.TabIndex = 2;
+            this.Test_Key.UseVisualStyleBackColor = true;
+            this.Test_Key.Click += new System.EventHandler(this.Test_Key_Click);
+            // 
+            // Enter_Key
+            // 
+            this.Enter_Key.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Enter_Key.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Enter_Key.Location = new System.Drawing.Point(79, 123);
+            this.Enter_Key.Name = "Enter_Key";
+            this.Enter_Key.PasswordChar = '*';
+            this.Enter_Key.Size = new System.Drawing.Size(113, 13);
+            this.Enter_Key.TabIndex = 3;
+            this.Enter_Key.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Enter_Key.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(92, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 13);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Safe Status";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.textBox2.Location = new System.Drawing.Point(79, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Enter The Key";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orange;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Enter_Key);
+            this.Controls.Add(this.Test_Key);
+            this.Controls.Add(this.Btn_Set_Combination);
+            this.Controls.Add(this.Txt_Status);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Txt_Status;
+        private System.Windows.Forms.Button Btn_Set_Combination;
+        private System.Windows.Forms.Button Test_Key;
+        private System.Windows.Forms.TextBox Enter_Key;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ The_Safe/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MyDialogs;
+namespace The_Safe
+{
+    public partial class Form1 : Form
+    {
+        String Str_Combination;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Btn_Set_Combination_Click(object sender, EventArgs e)
+        {
+            if (Txt_Status.Text == "UnLocked" || Txt_Status.Text == "Combination Not Set")
+            {
+                 this.BackColor = System.Drawing.Color.Orange;
+                    //this code brings up an input box for the user to put in new combination.
+                Str_Combination = My_Dialogs.InputBox("Please Enter The Combination That Is Greater Than Five Characters");
+
+                while (Str_Combination.Length < 6)
+                {       //this pops up an error message if they do not put in more than five characters.  
+                    MessageBox.Show("That Combination Is Wrong, Please Try Again.");
+                    Str_Combination = My_Dialogs.InputBox("Please Enter The Combination That Is Greater Than Five Characters");
+                }
+                Txt_Status.Text = "Locked";
+                Test_Key.Text = "Test Key";
+                this.Txt_Status.BackColor = Color.Red;
+                this.BackColor = System.Drawing.Color.Red;
+                this.Enter_Key.BackColor = Color.PeachPuff;
+                this.Txt_Status.BackColor = Color.PeachPuff;
+                Enter_Key.Clear();
+            }
+            else
+            {       //This message tells you to unlock the safe before trying to change the combination.
+                MessageBox.Show("You Must UnLock The Safe Before Trying To Change The Combination.");
+            }
+        }
+
+        private void Test_Key_Click(object sender, EventArgs e)
+        {
+            if (Test_Key.Text == "Test Key")
+            {
+                if (Enter_Key.Text == Str_Combination)
+                {   //this piece of code tells the user that if the text in the enter key box does equal the set combination then the status turns to UnLocked and turns the colour to green and green yellow.
+                    Txt_Status.Text = "UnLocked";
+                    this.Txt_Status.BackColor = Color.GreenYellow;
+                    this.Enter_Key.BackColor = Color.GreenYellow;
+                    this.BackColor = System.Drawing.Color.Green;
+                    Test_Key.Text = "Lock Safe";
+                    Enter_Key.Clear();
+                }
+                else
+                {   //this piece of code tells the user that if the text in the enter key box does not equal the set combination then a message box will pop up saying that the key is invalid and change colour to red and peach puff.
+                    MessageBox.Show("This Key Is Invalid");
+                    this.BackColor = System.Drawing.Color.Red;
+                    this.Txt_Status.BackColor = Color.PeachPuff;
+                    this.Enter_Key.BackColor = Color.PeachPuff;
+                }
+            }
+            else
+            {
+                Txt_Status.Text = "Locked";
+                Test_Key.Text = "Test Key";
+                this.Txt_Status.BackColor = Color.Red;
+                this.BackColor = System.Drawing.Color.Red;
+                this.Enter_Key.BackColor = Color.PeachPuff;
+                this.Txt_Status.BackColor = Color.PeachPuff;
+                Enter_Key.Clear();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Txt_Status_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
